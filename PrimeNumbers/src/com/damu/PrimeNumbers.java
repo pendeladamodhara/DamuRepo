818fc9113a943a36779739d93c4bd585e6c642c0
damu <damu@damu-PC>
2017-07-21 11:47:28 +0530

    Prime numbers project committed

---
+++ PrimeNumbers/src/com/damu/PrimeNumbers.java
@@ -0,0 +1,31 @@
+package com.damu;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter number to get prime numbers");
+	int num=Integer.parseInt(s.next());
+	int count;
+	for(int i=1; i<=num;i++)
+	{
+		count=0;
+		for(int j=2;j<=2;j++)
+		{
+			if(i%j==0)
+			{
+				count++;
+			}
+		}
+		if(i==100)
+		{
+			break;
+		}
+		if(count==0)
+		{
+			System.out.println(i);
+		}
+	}
+}
+}
