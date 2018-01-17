b4d09da54005b9fdc454e9df5f486ad8d2e10ea8
damodara <pendeladamodhara@gmail.com>
2018-01-17 23:33:11 +0530

    Data is committed

--- PrimeNumbers/src/com/damu/PrimeNumbers.java
+++ PrimeNumbers/src/com/damu/PrimeNumbers.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class PrimeNumbers {
 public static void main(String[] args) {
 	Scanner s=new Scanner(System.in);
-	System.out.println("enter number to get prime numbers");
+	System.out.println("enter number to get prime numbers========");
 	int num=Integer.parseInt(s.next());
 	int count;
 	for(int i=1; i<=num;i++)
 	{
+            
 		count=0;
 		for(int j=2;j<=2;j++)
 		{
