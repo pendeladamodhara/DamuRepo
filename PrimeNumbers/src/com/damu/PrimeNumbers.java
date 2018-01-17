7eed3dca1dbeba2a80268f92140db8ccf7f46780
damodara <pendeladamodhara@gmail.com>
2018-01-17 23:37:37 +0530

    added message

--- PrimeNumbers/src/com/damu/PrimeNumbers.java
+++ PrimeNumbers/src/com/damu/PrimeNumbers.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PrimeNumbers {
 public static void main(String[] args) {
 	Scanner s=new Scanner(System.in);
-	System.out.println("enter number to get prime numbers========");
+	System.out.println("enter number to get prime n  umbers========");
 	int num=Integer.parseInt(s.next());
 	int count;
 	for(int i=1; i<=num;i++)
